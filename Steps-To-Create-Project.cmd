b793ff2e630b7695f97dbcc239c68e05b9cc505b
SAIDEEPBHANGE <saideepbhange11022002@gmail.com>
2026-02-08 13:12:35 +0530

    * Commad line codes are Added * Folder Structure is Added * Server, Client, Shared Project is Created

---
+++ Steps-To-Create-Project.cmd
@@ -0,0 +1,27 @@
+﻿:: Create the main solution folder and enter it
+mkdir Task-Project-Management-System
+cd Task-Project-Management-System
+
+:: Create a new solution file
+dotnet new sln -n Task-Project-Management-System
+
+:: Create the Blazor WebAssembly project with interactivity
+dotnet new blazor --interactivity WebAssembly --all-interactive -n Task-Project-Management-System
+
+:: Add the server/host project to the solution
+dotnet sln add Task-Project-Management-System\Task-Project-Management-System.csproj
+
+:: Add the client project to the solution
+dotnet sln add Task-Project-Management-System.Client\Task-Project-Management-System.Client.csproj
+
+:: Create a Shared class library for DTOs / models / utils
+dotnet new classlib -n Shared
+
+:: Add the Shared project to the solution
+dotnet sln add Shared\Shared.csproj
+
+:: Add a reference from the Server project to the Shared project
+dotnet add Task-Project-Management-System\Task-Project-Management-System.csproj reference Shared\Shared.csproj
+
+:: Add a reference from the Client project to the Shared project
+dotnet add Task-Project-Management-System.Client\Task-Project-Management-System.Client.csproj reference Shared\Shared.csproj
---
+++ Task-Project-Management-System/Program.cs
@@ -0,0 +1,33 @@
+using Task_Project_Management_System.Client.Pages;
+using Task_Project_Management_System.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Task_Project_Management_System.Client._Imports).Assembly);
+
+app.Run();
